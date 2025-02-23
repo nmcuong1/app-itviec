fc4f814a08aaa286d7de4e454eaac1c6bf783d97
DELL <DELL@DESKTOP-UEPQA6V>
2025-02-24 02:21:40 +0700

    ,

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
       <Header />
       <Routes>  
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={HomePage} />
         {/* <Route path="/jobs" element={<JobPage />} />
         <Route path="/companies" element={<CompanyPage />} />*/}
       </Routes>
