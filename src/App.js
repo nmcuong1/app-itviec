5b9256e3c2f7719c1c226fa589c5f385d704acb6
DELL <DELL@DESKTOP-UEPQA6V>
2025-02-24 02:45:52 +0700

    .

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -7,17 +7,17 @@ import './assets/styles/App.css';
 
 function App() {
   return (
-    <BrowserRouter basename="/">
-    <div className="App">
-      <Header />
-      <Routes>  
-        <Route path="/" element={<HomePage />} />
-        {/* <Route path="/jobs" element={<JobPage />} />
-        <Route path="/companies" element={<CompanyPage />} />*/}
-      </Routes>
-      <Footer />
-    </div>
-  </BrowserRouter>
+    <Router basename="/app-itviec">
+      <div className="App">
+        <Header />
+        <Routes>  
+          <Route path="/" element={<HomePage />} />
+          {/* <Route path="/jobs" element={<JobPage />} />
+          <Route path="/companies" element={<CompanyPage />} /> */}
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
